b3b3af93a415402375a627d9ddcc20b103b66623
Amit72709 <amitkumarsingh72709@gmail.com>
2025-11-22 21:24:39 +0530

    server  port commit

--- backend/server.js
+++ backend/server.js
@@ -147,7 +147,7 @@ async function start() {
     initHandlers(io, socket);
   });
 
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 10000;
   server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
 
